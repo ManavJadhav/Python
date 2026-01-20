0fe19ff8ab9f1530e3813e8d6164c1f39d5fc0d1
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-21 00:25:23 +0530

    Jay Ganesh

---
+++ Tasks/Task_1/A9Q1.py
@@ -0,0 +1,19 @@
+
+###########################################################################
+#  Function name :     Display
+#  Input :             None
+#  Output :            None
+#  Description :       It used to display "Jay Ganesh" on screen.
+#  Author :            Manav Mahadev Jadhav
+#  Date :              20/01/2026
+#
+###########################################################################
+
+def Display():
+    print("Jay Ganesh...")
+
+def main():
+    Display()
+
+if __name__ == "__main__":
+    main()
