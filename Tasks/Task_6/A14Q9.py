7b2c742071ccc52b032c0d155145c6eab01f198e
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-23 21:29:41 +0530

    Implement lambda Multiplication function

---
+++ Tasks/Task_6/A14Q9.py
@@ -0,0 +1,33 @@
+##################################################################################################
+#  Lambda Function  :     LambdaMultiplication
+#  Input :                int , int
+#  Output :               int
+#  Description :          It is a lambda function used to return Multiplication of 2 numbers.
+#  Author :               Manav Mahadev Jadhav
+#  Date :                 22/01/2026
+#
+##################################################################################################
+
+# def LambdaMultiplication(No1,No2):
+#     return No1 * No2
+
+LambdaMultiplication = lambda No1, No2 : No1 * No2
+
+
+def main():
+    Value1 = 0
+    Value2 = 0
+    Ret = 0
+
+    print("Enter the first number : ")
+    Value1 = int(input())
+
+    print("Enter the second number : ")
+    Value2 = int(input())
+
+    Ret = LambdaMultiplication(Value1,Value2)
+    print("Multiplication is : ",Ret)
+    
+
+if __name__ == "__main__":
+    main()
