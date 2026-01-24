47c26ea82476bd1432081149949c41a03a1d954c
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-24 23:11:48 +0530

    Implement for loop

---
+++ Tasks/Task_8/A16Q4.py
@@ -0,0 +1,21 @@
+###########################################################################
+#  Function name :     Display
+#  Input :             None 
+#  Output :            None
+#  Description :       It used to display 5 times Marvellous on screen .
+#  Author :            Manav Mahadev Jadhav
+#  Date :              22/01/2026
+#
+###########################################################################
+
+def Display():
+    for i in range(5):
+        print("Marvellous")
+
+
+def main():
+
+    Display()
+
+if __name__ == "__main__":
+    main()
