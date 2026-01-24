9e408780f57d50fecb81eae6ea18599bf275d62f
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-24 23:18:39 +0530

    Implement Pattern Printing

---
+++ Tasks/Task_9/A17Q6.py
@@ -0,0 +1,30 @@
+###########################################################################
+#  Function name :     Display
+#  Input :             int 
+#  Output :            None
+#  Description :       It used to display pattern on screen.
+#  Author :            Manav Mahadev Jadhav
+#  Date :              22/01/2026
+#
+###########################################################################
+
+def Display(No):
+    for i in range(No,0,-1):
+        print()
+        for j in range(1,No+1):
+            if(i >= j):
+                print("*", end="\t")
+
+    print()
+
+def main():
+    Value = 0
+
+    print("Enter a number: ")
+    Value = int(input())
+
+    Display(Value)
+
+
+if __name__ == "__main__":
+    main()
