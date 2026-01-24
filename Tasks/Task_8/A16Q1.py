02ab088a70ea4b8a1b4540051d46fbf717c227d3
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-24 23:31:01 +0530

    Implement a simple print statement

---
+++ Tasks/Task_8/A16Q1.py
@@ -0,0 +1,20 @@
+###########################################################################
+#  Function name :     Fun
+#  Input :             None 
+#  Output :            None
+#  Description :       It used to display statement from Fun function.
+#  Author :            Manav Mahadev Jadhav
+#  Date :              22/01/2026
+#
+###########################################################################
+
+def Fun():
+    print("Hello from Fun")
+
+
+def main():
+
+    Fun()
+
+if __name__ == "__main__":
+    main()
