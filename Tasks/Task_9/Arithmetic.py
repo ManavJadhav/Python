405cf122b5049ca984ad798439dde782a717cb96
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-24 23:38:38 +0530

    Arithmetic Module

---
+++ Tasks/Task_9/Arithmetic.py
@@ -0,0 +1,19 @@
+def Addition(No1, No2):
+    Sum = 0
+    Sum = No1 + No2
+    return Sum
+
+def Subtraction(No1, No2):
+    Sub = 0
+    Sub = No1 - No2
+    return Sub
+
+def Multiplication(No1, No2):
+    Mult = 0
+    Mult = No1 * No2
+    return Mult
+
+def Division(No1, No2):
+    Div = 0
+    Div = No1 / No2
+    return Div
