e3c0f7fa3acf93403d51e52688de2c334bdcce36
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-27 23:40:08 +0530

    Implement inbuilt getsizeof function

---
+++ Datatypes & InputOutput/Size.py
@@ -0,0 +1,13 @@
+import sys 
+
+No = 11545456468421564454564546546545555        # int
+
+Marks = 90.90   # float
+
+Name = "Rahul"   #str
+
+print(sys.getsizeof(No))
+
+print(sys.getsizeof(Marks))
+
+print(sys.getsizeof(Name))
