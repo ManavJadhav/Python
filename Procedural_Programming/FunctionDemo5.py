8add43bab804afe98d397ce179c28e781532aea3
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-30 22:45:00 +0530

    Implement function accepting single parameter

---
+++ Procedural_Programming/FunctionDemo5.py
@@ -0,0 +1,11 @@
+# Accept : 1 Paramter
+# Return : Nothing
+def FunctionBasicsDemo(Value):
+    print("Inside FunctionBasicsDemo :", Value)
+
+def main():
+    FunctionBasicsDemo("Python")
+    FunctionBasicsDemo(51)
+
+if __name__ == "__main__":
+    main()
