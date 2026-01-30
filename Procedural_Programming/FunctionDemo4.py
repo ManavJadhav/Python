51d84aa7eeecc620a19b3ea1f2cb6d9b87118a0d
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-30 22:44:27 +0530

    Implement function with no parameters and no return value

---
+++ Procedural_Programming/FunctionDemo4.py
@@ -0,0 +1,10 @@
+# Accept : Nothing
+# Return : Nothing
+def FunctionBasicsDemo():
+    print("Inside FunctionBasicsDemo ")
+
+def main():
+    FunctionBasicsDemo()
+
+if __name__ == "__main__":
+    main()
