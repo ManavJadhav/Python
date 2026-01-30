85d348fb70b7d4de9e6c6226e41ca29e13fbfe57
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-30 22:45:21 +0530

    Implement function accepting multiple parameters without return value

---
+++ Procedural_Programming/FunctionDemo6.py
@@ -0,0 +1,10 @@
+# Accept : Multiple Paramters
+# Return : Nothing
+def FunctionBasicsDemo(Value1 , Value2):
+    print("Inside FunctionBasicsDemo :", Value1,Value2)
+
+def main():
+    FunctionBasicsDemo("Python",21)
+
+if __name__ == "__main__":
+    main()
