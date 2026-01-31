40f2742bbe550ac0ed7d1b3c689a9a678a34eb85
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-31 23:32:27 +0530

    Implement client program to use user-defined module

---
+++ Modules/ModuleClient.py
@@ -0,0 +1,12 @@
+import Numbers
+
+print("Inside Client : ",__name__)
+print("Value of PI is  : ",Numbers.PI)
+
+Result = 0
+
+Result = Numbers.Add(11,10)
+print("Addition is : ", Result)
+
+Result = Numbers.Sub(11,10)
+print("Substraction  is : ", Result)
