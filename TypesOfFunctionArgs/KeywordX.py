3974e363cc064c94d36002556c64b2a40ac29073
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-31 23:21:12 +0530

    Implement keyword arguments with None value

---
+++ TypesOfFunctionArgs/KeywordX.py
@@ -0,0 +1,12 @@
+def EmpolyeeInfo(Name , Age, Salary ,City):
+    print("Name : ",Name)
+    print("Age : ",Age)
+    print("Salary : ",Salary)
+    print("City : ",City)
+
+def main():
+   # Keyword Arguments
+   EmpolyeeInfo(Age=26,Name="Rahul",City="Pune",Salary=None) # Correct
+
+if __name__ == "__main__":
+    main()
