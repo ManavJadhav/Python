79062a19bb704d71214ad2243625a7101083f5c8
ManavJadhav99 <manavjdhv@gmail.com>
2026-01-31 23:10:44 +0530

    Implement function using default arguments

---
+++ TypesOfFunctionArgs/Default.py
@@ -0,0 +1,11 @@
+def EmpolyeeInfo(Name="Gotya", Age =26 , Salary =1000 ,City = "Mumbai"):
+    print("Name : ",Name)
+    print("Age : ",Age)
+    print("Salary : ",Salary)
+    print("City : ",City)
+
+def main():
+   EmpolyeeInfo() 
+
+if __name__ == "__main__":
+    main()
