5aff48483ed9b149138855a411cbef287c18ea24
ManavJadhav99 <manavjdhv@gmail.com>
2026-02-21 22:13:14 +0530

    Implement basic directory automation workflow

---
+++ Automation/DirectoryAutomation1.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+def DirectoryScanner(DirName = "Demo"):
+    Ret = False
+
+    Ret = os.path.exists(DirName)
+    if(Ret == False):
+        print("there is no such directory")
+        return
+    
+    Ret = os.path.isdir(DirName)
+    if(Ret == False):
+        print("It is not a directory")
+        return
+    
+    for FolderName,SubFolder,FileName in os.walk(DirName):
+
+        for fname in FileName :
+            print(fname)
+
+def main():
+    Border = "-" * 50
+
+    print(Border)
+    print("-------- Directory Automation ---------")
+    print(Border)
+
+    if(len(sys.argv) != 2):
+        print("Invalid number of arguments")
+        print("Please specify the name of directory ")
+        return
+
+    DirectoryScanner(sys.argv[1])
+
+if __name__ =="__main__":
+    main()
